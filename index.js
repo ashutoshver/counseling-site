edca4e98c0fe0c75c5fa6b076227499791cc8727
ashutoshver <ashutoshrock24@gmail.com>
2023-05-11 16:09:51 +0530

    updateall

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ function popupfunc() {
   popup.classList.toggle("show");
 }
 
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", navToggle);
+
+function navToggle() {
+  navToggler.classList.toggle("active");
+  const nav = document.querySelector(".nav");
+  nav.classList.toggle("open");
+  if (nav.classList.contains("open")) {
+    nav.style.maxHeight = nav.scrollHeight + "600px";
+  }
+  else {
+    nav.removeAttribute("style");
+  }
+} 
